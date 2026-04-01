ee3c13674c67ee21c19d655b69590b54d32975e9
Diogo <diogo.rgalvao@outlook.com>
2026-04-01 15:03:26 -0300

    bug fix

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   compiler: { styledComponents: true },
-  outputFileTracingRoot: require("path").join(__dirname, "../../"),
 };
 
 export default nextConfig;
